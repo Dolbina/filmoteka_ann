72189473927752f726a5a068c932aa615658bfe6
Ann Dolbina <dolbinaann@gmail.com>
2023-03-31 19:32:14 +0300

    add css

--- src/index.js
+++ src/index.js
@@ -58,10 +58,10 @@ function onSearchImg(event) {
 
 // Розмітка для 1 шт. фото
 const createImg = item =>
-  `<a class="photo-link" href="${item.largeImageURL}">
+  `<a class="photo-link" href="${item.poster_path}">
   <div class="photo-card">
   <div class="photo-wrap">
-  <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+  <img src="${item.poster_path}" alt="${item.title}" loading="lazy" />
   </div>
   <div class="info">
     <p class="info-item">
@@ -96,17 +96,28 @@ const insertContent = (array) => {
 
 // запрос зображення
 async function fetchImg(name, page) {
-  const API_URL = 'https://pixabay.com/api/';
+  const API_URL =
+    'https://api.themoviedb.org/3/trending/movie/day?api_key=<<api_key>>';
   const options = {
-    params: {
-      key: '34361382-9628d27261ff8745ccc230a20',
-      q: name,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: 'true',
-      page: page,
-      per_page: 40,
-    },
+    results: [
+      {
+        adult: false,
+        backdrop_path: '/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg',
+        genre_ids: [28, 12, 14, 878],
+        id: 299536,
+        original_language: 'en',
+        original_title: 'Avengers: Infinity War',
+        overview:
+          'As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.',
+        poster_path: '/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg',
+        release_date: '2018-04-25',
+        title: 'Avengers: Infinity War',
+        video: false,
+        vote_average: 8.3,
+        vote_count: 6937,
+        popularity: 358.799,
+      },
+    ],
   };
   try {
     const response = await axios.get(API_URL, options);
